05b0b95ffb7650bc4a8629cd1df19b4697142377
Liv_Alles <38350@ma-web.nl>
2025-05-12 13:04:08 +0200

    dash
    there is also double jump code in here but it doesnt work yet thats  an issue for wensday

--- periode 4 mythe opdracht/Assets/scripts/PlayerMovement.cs
+++ periode 4 mythe opdracht/Assets/scripts/PlayerMovement.cs
@@ -8,31 +8,66 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    //dash
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 12f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 2f;
+    //double jump
+    private bool doubleJump;
+
+
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
     // Start is called before the first frame update
 
 
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+        //makes sure double jump only activates in the air
+        if (IsGrounded() && Input.GetButton("Jump"))
+        { 
+            doubleJump = false;
+        }
+
         //movement
         horizontal = Input.GetAxisRaw("Horizontal");
         //makes you jump
-        if(Input.GetButtonDown("Jump")&& IsGrounded())
+        if(Input.GetButtonDown("Jump"))
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            if (IsGrounded() || doubleJump)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+
+                doubleJump = !doubleJump;
+            }
         }
         //allows you to jump higher the longer you press
         if (Input.GetButtonDown("Jump") && rb.linearVelocity.y > 0f)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
         Flip();
     }
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return; 
+        }
         //more movement
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
     }
@@ -51,5 +86,20 @@ public class PlayerMovement : MonoBehaviour
             localscale.x *= -1f;
             transform.localScale = localscale;
         }
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float orignalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = orignalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
