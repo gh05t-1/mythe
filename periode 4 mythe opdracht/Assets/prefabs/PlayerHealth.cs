de82682c2f600a996f43b0d01ec8ec677ea61d52
Skyler <39634@ma-web.nl>
2025-05-21 10:39:21 +0200

    HealthSlider

---
+++ periode 4 mythe opdracht/Assets/prefabs/PlayerHealth.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 100;
+    public Slider Slider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+        Slider.maxValue = maxHealth;
+        Slider.value = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        Slider.value = health;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
