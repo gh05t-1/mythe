185fa5f5dcf7f11c202dd60f1779fa7b13d6fe15
Liv_Alles <38350@ma-web.nl>
2025-05-22 18:44:31 +0200

    dialogue

---
+++ periode 4 mythe opdracht/Assets/NPC.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    void Start()
+    {
+        dialogueText.text = "";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return) && playerIsClose) 
+        { 
+            if(dialoguePanel.activeInHierarchy)
+            {
+                ZeroText();
+            }else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        if(dialogueText.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+    public void ZeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false); 
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds (wordSpeed);
+        }
+    }
+    
+    public void NextLine()
+    {
+        contButton.SetActive (false);
+        if(index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+
+        }
+        else
+        {
+            ZeroText ();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            ZeroText();
+        }
+    }
+}
