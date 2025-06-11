3362c6e82c96778b17199a495fe6aeeaab6c053f
nessieport <160978223+nessieport@users.noreply.github.com>
2025-06-11 13:09:56 +0200

    fish

--- periode 4 mythe opdracht/Assets/scripts/parralax.cs
+++ periode 4 mythe opdracht/Assets/scripts/parralax.cs
@@ -17,8 +17,12 @@ public class parralax : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        float temp = (cam.transform.position.x * (1 - parralaxEffect));
         float dist = (cam.transform.position.x * parralaxEffect);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
